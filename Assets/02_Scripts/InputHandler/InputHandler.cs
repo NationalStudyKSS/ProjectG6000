a41859396531cc86c03a38f9290f8584cdb90601
opopyyn <endsun1234@naver.com>
2025-08-06 19:54:56 +0900

    sdf

---
+++ Assets/02_Scripts/InputHandler/InputHandler.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+/// <summary>
+/// 입력을 받는 클래스
+/// </summary>
+public abstract class InputHandler : MonoBehaviour
+{
+    public event Action<Vector2> OnMove; // 이동 이벤트
+
+}
---
+++ Assets/02_Scripts/InputHandler/PlayerInputHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 플레이어의 입력을 받아 처리하는 클래스
+/// </summary>
+public class PlayerInputHandler : InputHandler
+{
+
+}
